ead70ad7898ccac5caefdb6b08e84d826462ed9c
Lucas <lukasmierez@gmail.com>
2024-12-04 13:53:10 -0300

    readme

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,26 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+
+import { Navbar } from "../ui";
+
+import { MarvelPage, DcPage } from "../heroes"
+import { LoginPage } from "../auth"
+
+
+
+export const AppRouter = () => {
+  
+  return (
+    <>
+        <Navbar />
+
+        <Routes>
+            <Route path="marvel" element={<MarvelPage />} />
+            <Route path="dc" element={<DcPage />} />
+            
+            <Route path="login" element={<LoginPage />} />
+            
+            <Route path="/" element={<Navigate to={"/marvel"} />} />
+        </Routes>
+    </>    
+  )
+};
